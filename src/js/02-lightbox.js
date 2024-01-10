568c72477302e9950c4a242c7ad8c02aba691b1d
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2024-01-10 17:12:50 +0000

    task2_completed

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryDiv = document.querySelector(".gallery");
+const markup = (images) => {
+  return images
+    .map((image) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href=${image.original}>
+                <img class="gallery__image" src=${image.preview} alt=${image.description} title=${image.description}/>
+            </a>
+        </li>
+        `;
+    })
+    .join("");
+};
+galleryDiv.insertAdjacentHTML("afterbegin", markup(galleryItems));
+let gallery = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
